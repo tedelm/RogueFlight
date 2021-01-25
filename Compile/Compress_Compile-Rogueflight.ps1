ccdd079fb58722f9d2f32c1d363046cf6a1e6cca
Ted Elm <ted.elmenheim@gmail.com>
2021-01-25 23:18:39 +0100

    0.3.1001

--- Compile/Compress_Compile-Rogueflight.ps1
+++ Compile/Compress_Compile-Rogueflight.ps1
@@ -1,14 +1,27 @@
 
 
-$files = get-childitem "C:\GITHUB\RogueFlight\RogueFlight\Releases\Pre-Alpha\0.2.1001"
 
 
-cd C:\Tools\7-ZipPortable\App\7-Zip\
+$Version = "0.3.1001_"
+
+$files = Get-ChildItem "C:\GITHUB\RogueFlight\RogueFlight\Firmware\obj" -Filter "*.hex"
 
 
 foreach($file in $files){
 
-    .\7z.exe a "C:\GITHUB\RogueFlight\RogueFlight\Releases\Pre-Alpha\0.2.1001\$($file.name).zip" "$($file.fullname)"
+    $Newfilename = ($($file.name) -split "_")[2]
+    $Newfilename = "RogueFlight_$Version$Newfilename.hex"
+    $Newfilename
+    Rename-Item -Path "$($file.fullname)" -NewName "$($Newfilename)"
+}
+
+cd C:\Tools\7-ZipPortable\App\7-Zip\
+
+$files = Get-ChildItem "C:\GITHUB\RogueFlight\RogueFlight\Firmware\obj" -Filter "*.hex"
+
+foreach($file in $files){
+
+    .\7z.exe a "C:\GITHUB\RogueFlight\RogueFlight\Releases\Pre-Alpha\0.3.1001\$($file.name).zip" "$($file.fullname)"
 }
 
  
